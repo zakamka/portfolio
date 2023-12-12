5eb1eb0693d05e943bcd9404bf4d084f0bbe69de
zakamka <na@bulygin.su>
2023-12-12 22:52:10 +0300

    3

--- app.js
+++ app.js
@@ -47,3 +47,25 @@ function HidePopup() {
     popup.style = "visibility:hidden;"
     isHidden = true;
 }
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
